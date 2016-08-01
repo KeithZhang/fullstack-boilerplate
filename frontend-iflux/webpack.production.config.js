f9dddda6b58c861880c51e4875b96bcf69a48a3b
KeithZhang <g.fengling.11@gmail.com>
2016-08-01 17:58:08 +0800

    增加配置

---
+++ frontend-iflux/webpack.production.config.js
@@ -0,0 +1,42 @@
+var path = require('path');
+var webpack = require('webpack');
+var packageJson = require('./package.json')
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+  entry: [ 
+    './src/index.js'
+  ],
+  output: {
+    filename: packageJson.name + '.js',
+    path: path.join(__dirname, 'dist'),
+    publicPath: ''
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new ExtractTextPlugin("[name].css"),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel?cacheDirectory'
+      },
+      {
+        test: /\.less$/,
+        exclude: /node_modules/,
+        loader: ExtractTextPlugin.extract('style', 'css!less')
+      }
+    ]
+  }
+};
